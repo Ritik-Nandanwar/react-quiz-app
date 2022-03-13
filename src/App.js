eeb607d71869beb8a1473842f8ff3829c94b5b33
Ritik Nandanwar <nandanwarritik10@gmail.com>
2022-03-13 20:03:45 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+import Home from "./Components/Home";
+function App() {
+  return (
+    <BrowserRouter>
+      <Home />
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/Components/Home.js
@@ -0,0 +1,7 @@
+import React, { useContext, useState } from "react";
+const Home = () => {
+  return <div className="">
+    HOME</div>
+};
+
+export default Home;
